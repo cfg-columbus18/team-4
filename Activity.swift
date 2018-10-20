5ebb58957b75a16548248de27c6907bc35dcc477
sadaqat12 <32377500+sadaqat12@users.noreply.github.com>
2018-10-20 02:49:31 -0400

    Add files via upload

---
+++ Activity.swift
@@ -0,0 +1,20 @@
+//
+//  Activity.swift
+//  firstone
+//
+//  Created by Sadaqat Ali on 10/20/18.
+//  Copyright © 2018 Sadaqat Ali. All rights reserved.
+//
+
+import Foundation
+
+struct Activity {
+    var activityName: String = ""
+    var value: Int = 0
+    
+    init (activityName: String, value: Int) {
+        activityName = activityName
+        value = value
+    }
+    
+}
